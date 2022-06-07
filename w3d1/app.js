d3d2d710fcc44da7e95a05f07c6360e129daa7cd
ferturk <102708435+ferturk@users.noreply.github.com>
2022-06-07 08:43:59 -0500

    nodejsassignment

---
+++ w3d1/app.js
@@ -0,0 +1,7 @@
+var arrayobjectexercise = require('./nodejsassignment.js');
+let array=[2,1,5,6,7,4];
+console.log(arrayobjectexercise.findevennumbers(array));
+console.log(arrayobjectexercise.findoddnumbers(array));
+console.log(arrayobjectexercise.findDivisible4NumbersSum(array));
+let obj={a:1, b:2};
+console.log(arrayobjectexercise.objecttoarray(obj));
---
+++ w3d1/nodejsassignment.js
@@ -0,0 +1,198 @@
+function findoddnumbers(array) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        if ((i % 2) != 0) {
+            result.push(array[i]);
+        }
+    }
+    return result;
+};
+
+function findevennumbers(array) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        if ((array[i] % 2) == 0) {
+            result.push(array[i]);
+        }
+    }
+    return result;
+};
+
+function finddivisible4(array) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        if ((array[i] % 4) == 0) {
+            result.push(array[i]);
+        }
+    }
+    return result;
+};
+
+function findOddNumbersSum(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        if ((array[i] % 2) != 0) {
+            sum += array[i];
+        }
+    }
+    return sum;
+};
+
+function findEvenNumbersSum(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        if ((array[i] % 2) == 0) {
+            sum += array[i];
+        }
+    }
+    return sum;
+};
+
+function findDivisible4NumbersSum(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        if ((array[i] % 4) == 0) {
+            sum += array[i];
+        }
+    }
+    return sum;
+};
+
+function finduniquenumbers(array) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (result.indexOf(array[i]) == -1) {
+            result.push(array[i])
+        }
+    }
+    return result;
+};
+
+function findtypeofarray(array) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        result.push(typeof arr[i]);
+    }
+    return result;
+};
+
+function destructureArr(array) {
+    let result = {};
+    for (let i = 0; i < array.length; i++) {
+        result[typeof array[i] + i] = array[i];
+    }
+    return result;
+};
+
+function firstOddNumberIndex(arr) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 != 0) return i;
+    }
+};
+
+function stringtoarray(string) {
+    let result = [];
+    for (let i = 0; i < string.length; i++) {
+        result.push(string.charAt(i));
+    }
+    return result;
+};
+
+function arraytostring(array) {
+    let str = "";
+    for (let i = 0; i < array.length; i++) {
+        str += array[i];
+    }
+    return str;
+};
+
+function objecttoarray(object) {
+    let objentries = Object.entries(object);
+    let objArr = [];
+    for (let [key, value] of objentries) {
+        let x;
+        if (Array.isArray(value)) {
+            value = value.reduce((accumulator, element) => {
+                return accumulator += element;
+
+            }, '');
+            x = key + "" + value;
+            objArr.push(x);
+        }
+        else if (typeof (value) == 'object') {
+            value = convertObjectToArray(value)
+            for (let y of value) {
+                objArr.push(key + "" + y);
+            }
+        } else{
+            x = key + "" + value;
+            objArr.push(x);
+        }
+    }
+    return objArr;
+}
+
+
+function minifyObject(array){
+    let result = [];
+    for(let item of array){
+        for(let key in item){
+            if(key == "a"){
+                result.push({[key]:item[key]});
+            }
+        }
+    }
+    return result;
+};
+
+function findoldestpersonage(array){
+    let oldestAge = 0;
+    for(let person of array){
+        if(person.age > oldestAge){
+            oldestAge = person.age;
+            oldestName = person.name;
+        }
+    }
+    return oldestAge;
+};
+
+function findoyoungestpersonname(array){
+    let youngestName = "";
+    let youngestAge = 0;
+    for(let person of array){
+        if(person.age < youngestAge){
+            youngestAge = person.age;
+            youngestName = person.name;
+        }
+    }
+    return youngestName;
+};
+
+function findbetween(array){
+    for(let person of array){
+        if(person.age >= 16 && person.age <=17){
+            return person;
+        }
+    }
+}
+
+
+module.exports = {
+    findoddnumbers: findoddnumbers,
+    findevennumbers: findevennumbers,
+    finddivisible4: finddivisible4,
+    findOddNumbersSum: findOddNumbersSum,
+    findEvenNumbersSum: findEvenNumbersSum,
+    findDivisible4NumbersSum: findDivisible4NumbersSum,
+    finduniquenumbers: finduniquenumbers,
+    findtypeofarray: findtypeofarray,
+    destructureArr: destructureArr,
+    firstOddNumberIndex: firstOddNumberIndex,
+    stringtoarray: stringtoarray,
+    arraytostring: arraytostring,
+    objecttoarray: objecttoarray,
+    minifyObject:minifyObject,
+    findoldestpersonage:findoldestpersonage,
+    findoyoungestpersonname:findoyoungestpersonname,
+    findbetween:findbetween
+}
