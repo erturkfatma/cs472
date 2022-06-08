34121289f4ef71fda2b508739d9b8004af21a820
erturkfatma <102713499+erturkfatma@users.noreply.github.com>
2022-06-08 08:19:59 -0500

    w3d2

--- w3d2/server.js
+++ w3d2/server.js
@@ -9,54 +9,55 @@ app.use(
 
 
 
-let data ={};
+let data = {};
 
-app.get("/", (req,res) => {
-   res.sendFile(__dirname + "/public" + "/domassignment.html")
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "" + "/domassignment.html")
 })
 
-app.post("/Add", (req , res)=>{
-    console.log(req.body);
-    data=req.body;
-    res.redirect("/view");
+app.post("/Add", (req, res) => {
+  console.log(req.body);
+  data = req.body;
+  res.redirect("/view");
 });
 
-app.get("/view",(req,res) => {
+app.get("/view", (req, res) => {
   res.send(
-   "<label>text1: " +
-     data.text1 +
-     "</label>" +
-     "<br>" +
-     "<label>Radio: " +
-     data.radio1 +
-     "</label>" +
-     "<br>" +
-     "<label>Radio: " +
-     data.radio2 +
-     "</label>" +
-     "<label>Radio: " +
-     data.radio3 +
-     "</label>" +
-     "<label>Checkbox: " +
-     data.checkbox1 +
-     "</label>" +
-     "<br>" +
-     "<label>Selected Value: " +
-     data.selectedValue +
-     "</label>" +
-     "<br>" +
-     "<label>Text Area: " +
-     data.textArea +
-     "</label>"
- );
+    "<label>text1: " +
+    data.text1 +
+    "</label>" +
+    "<br>" +
+    "<label>Radio: " +
+    data.radio1 +
+    "</label>" +
+    "<br>" +
+    "<label>Radio: " +
+    data.radio2 +
+    "</label>" +
+    "<label>Radio: " +
+    data.radio3 +
+    "</label>" +
+    "<label>Checkbox: " +
+    data.check1 +
+    "</label>" +
+    "<label>Checkbox: " +
+    data.check2 +
+    "</label>" +
+    "<label>Checkbox: " +
+    data.check3 +
+    "</label>" +
+    "<label>Textarea: " +
+    data.textarea1 +
+    "</label>" +
+    "<br>"
+  );
 })
 
 
 app.use((req, res) => {
-   res.sendFile(__dirname + "/public" + "/notfound.html")
+  res.sendFile(__dirname + "/public" + "/notfound.html")
 })
 
 app.listen(3000, () => {
-   console.log('Your Server is running on 3000');
+  console.log('Your Server is running on 3000');
 })
- 
